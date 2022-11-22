4b0689ffdbd4b7017d9e75211f1347c35b42651b
Samuli Saukkonen <ssaukkonen@gmail.com>
2022-11-22 19:22:46 +0200

    Created a small adding program and it's tests.

--- program.py
+++ program.py
@@ -1,5 +1,9 @@
-def main():
-    pass
+def main(int1, int2):
+    try:
+        return int1 + int2
+    except Exception as e:
+        print(e)
+        return 0
 
 if __name__=="main":
     main()  # pragma: no cover
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,10 @@ from .. import program
 
 
 class TestProgram:
-    def test_main_function_runs(self):
-        program.main()
+    def test_adding_numbers_works(self):
+        assert program.main(1, 1) == 2
+        assert program.main(2, 1) == 3
+        assert program.main(3, 3) == 6
+
+    def test_returns_0_if_there_is_error(self):
+        assert program.main(1, '1') == 0
